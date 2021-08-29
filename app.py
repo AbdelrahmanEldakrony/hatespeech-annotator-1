31a25592042855dba621a55dc93f2545aa320344
Abdelrahman10 <eldakrony@kanal-netz.de>
2021-08-29 14:19:52 +0200

    typos

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ img_path = './img/'
 
 # ----- Annotation -----
 if selected_option == option2: 
-    st.markdown("<h1 style='text-align: center;'>Hellooo! Please label some of the memes below</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Hello! Please label some of the memes below</h1>", unsafe_allow_html=True)
 
     results = []
 
